fa607b88c33a3d8a13920772f240ee410dfc3a92
Karthik <karthikreddy.chinasani1995@gmail.com>
2018-12-17 20:00:26 +0530

    Added bycrypt classes

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,29 +1,37 @@
-const registration_Service = require("../services/registerService");
-
-exports.registerUser = (req,res) => {
-  console.log(req);
-    registration_Service.registerUser(req.body).then(result => {
+const registrationService = require("../services/registerService");
+const bcrypt = require("bcrypt");
+const CONSTS = require("../constants");
+exports.registerUser = (req, res) => {
+  var user = req.body;
+  bcrypt.hash(user.password, CONSTS.SALT_ROUNDS, function(err, hash) {
+    user.password = hash;
+    registrationService
+      .registerUser(req.body)
+      .then(result => {
         res.send(result);
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  });
+};
+
+exports.login = (req,res) => {
+  registrationService.validateUserName(req.body.userName, req.body.password).then(user => {
+      res.send(user);
     })
     .catch(err => {
       res.send(err);
+    });
+};
+
+exports.getUser = (req, res) => {
+  registrationService
+    .getUserDetails(req.params.id)
+    .then(result => {
+      res.send(result);
     })
-}
-
-exports.login = (req,res) => {
-  registration_Service.validateUserName(req.body.userName, req.body.password).then(user => {
-    res.send(user);
-  })
-  .catch(err => {
-    res.send(err);
-  })
-}
-
-exports.getUser = (req,res) => {
-  registration_Service.getUserDetails(req.params.id).then(result => {
-    res.send(result);
-  })
-  .catch(err => {
-    res.send(err);
-  })
-}
+    .catch(err => {
+      res.send(err);
+    });
+};
--- services/registerService.js
+++ services/registerService.js
@@ -1,86 +1,94 @@
 const Registration = require("../models/registrationModel");
 var config = require("../config/session");
-var jwt = require("jsonwebtoken"),
-secret = config.secret;
+var jwt = require("jsonwebtoken");
+var secret = config.secret;
+const bcrypt = require("bcrypt");
 
-function verifyUserNameExist(user){
+function verifyUserNameExist(user) {
   return new Promise(function(resolve, reject) {
-    Registration.find({ $or: [ {userName: user.userName} , {email: user.email} ] })
-    .then(users => {
-      if(users.length){
+    Registration.find({
+      $or: [{ userName: user.userName }, { email: user.email }]
+    }).then(users => {
+      if (users.length) {
         return reject({
           code: 400,
           message: "Email or username already exist"
-        })
-      } else{
-        return resolve(user)
+        });
+      } else {
+        return resolve(user);
       }
-    })
+    });
   });
 }
 
-function registerUser(user){
+function registerUser(user) {
   return new Promise((resolve, reject) => {
-    verifyUserNameExist(user).then(user => {
-      const registration = new Registration(user);
-      registration.save()
+    verifyUserNameExist(user)
+      .then(user => {
+        const registration = new Registration(user);
+        registration
+          .save()
           .then(data => {
-              return resolve({
-                message: "user registered successfully"
-              })
+            return resolve({
+              message: "user registered successfully"
+            });
           })
           .catch(err => {
-              return reject({
-                  message: "unable to save to database"
-              })
-          })
-    })
-    .catch(err => {
-      return reject(err)
-    })
-  })
-}
-
-function validateUserName(userName, password){
-  return new Promise ((resolve, reject) => {
-    Registration.findOne({ $or: [ { userName: userName }, { email: userName } ] } )
-    .then(user => {
-      if(user){
-        authenticateUser(user, password)
-        .then((user) => {
-          return resolve(getAuthenticatedResponse(user));
-        })
-        .catch(err => {
-          return reject(err);
-        })
-      }
-      else{
-        return reject({
-          message: "Invalid UserName",
-          status: 402
-        });
-      }
-    })
-    .catch(err => {
-      return reject(err);
-    })
-  })
-}
-
-function authenticateUser(user, password){
-  return new Promise((resolve, reject) => {
-    if(password != user.password){
-      return reject({
-        code: 403,
-        message: "Invalid Password"
+            return reject({
+              message: "unable to save to database"
+            });
+          });
       })
-    }
-    return resolve(user);
+      .catch(err => {
+        return reject(err);
+      });
   });
 }
 
-function getAuthenticatedResponse(user){
-  if(!user){
+function validateUserName(userName, password) {
+  return new Promise((resolve, reject) => {
+    Registration.findOne({ $or: [{ userName: userName }, { email: userName }] })
+      .then(user => {
+        if (user) {
+          authenticateUser(user, password)
+            .then(user => {
+              return resolve(getAuthenticatedResponse(user));
+            })
+            .catch(err => {
+              return reject(err);
+            });
+        } else {
+          return reject({
+            message: "Invalid UserName",
+            status: 402
+          });
+        }
+      })
+      .catch(err => {
+        return reject(err);
+      });
+  });
+}
+
+function authenticateUser(user, password) {
+  console.log(user, 'user');
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, user.password, function(err, res) {
+      console.log(res, 'res');
+      if (res) {
+        return resolve(user);
+      } else {
+        return reject({
+          code: 403,
+          message: "Invalid Password"
+        });
+      }
+    });
+  });
+}
+
+function getAuthenticatedResponse(user) {
+  if (!user) {
     return null;
   }
   var userData = user;
@@ -104,32 +112,33 @@ function authenticate(userData) {
   };
   // return token and user information with a refresh token
   var options = {
-      expiresIn: 86400 // expires in 24 hours
-    };
+    expiresIn: 86400 // expires in 24 hours
+  };
 
   return jwt.sign(payload, secret, options);
 }
 
-function getUserDetails(id){
+function getUserDetails(id) {
   return new Promise((resolve, reject) => {
-    Registration.findOne({_id: id}).then(user => {
+    Registration.findOne({ _id: id })
+      .then(user => {
         return resolve(userDetails(user));
-    })
-    .catch(err => {
-      return reject(err);
-    })
-  })
+      })
+      .catch(err => {
+        return reject(err);
+      });
+  });
 }
 
-function userDetails(user){
-    var data = {};
-    data.userName = user.userName;
-    data.firstName = user.firstName;
-    data.lastName = user.lastName;
-    data.email = user.email;
-    data.id = user.id;
-    data.create_date = user.create_date;
-    return data;
+function userDetails(user) {
+  var data = {};
+  data.userName = user.userName;
+  data.firstName = user.firstName;
+  data.lastName = user.lastName;
+  data.email = user.email;
+  data.id = user.id;
+  data.create_date = user.create_date;
+  return data;
 }
 
 module.exports = {
@@ -137,4 +146,4 @@ module.exports = {
   verifyUserNameExist,
   validateUserName,
   getUserDetails
-}
+};
